443a5ca157b40e95a8e22bcdf9fef451c578a28c
ngdeqi <deqi.angel@gmail.com>
2022-09-16 17:26:19 +0800

    Add streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -18,10 +18,13 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+        System.out.println("All data: ");
+        printDataUsingStream(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Printing deadlines");
+        printDeadlinesUsingStream(tasksData);
+
+        System.out.println("Total number of deadlines: " + countDeadlinesUsingStream(tasksData));
 
     }
 
@@ -35,17 +38,40 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
+
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using loop: ");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDataUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Print deadlines using loops: ");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.parallelStream()
+                .filter(n -> n instanceof Deadline)
+                .forEach(System.out::println);
     }
 }
