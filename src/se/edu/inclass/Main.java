3bc759a3b1817c214a9183f4c6052c5a69d604f8
zhoukaiwen <e0774651@u.nus.edu>
2022-09-16 17:01:58 +0800

    Add explanatory comments

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,8 +15,10 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+//        System.out.println("All data:");
+//        printData(tasksData);
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
